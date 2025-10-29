162bf75c94c045e39c25711f6fc2713ce360551c
charisma tubagus <charisma.tubagus@gmail.com>
2025-10-29 21:07:18 +0700

    Add GSL Microsoft

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,13 @@
 #include <vulkan/vulkan.h>
 #include <glm/glm.hpp>
 #include <GLFW/glfw3.h>
+#include <cstdint>
+#include <cmath>
+#include <gsl/gsl>
 
-int main() {
+using namespace std;
+using namespace gsl;
+int main(int32_t argc, zstring *argv) {
 	std::cout << "Hello world Charisma";
 	GLFWwindow* window;
 	return 0;
